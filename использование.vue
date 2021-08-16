2f04d4f1abb375cf64f0a108b92452521ccdbd34
Borissenko_Nick <016@rambler.ru>
2021-08-16 10:07:56 +0300

    16aug21

---
+++ использование.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ $t('message.hello', { name: 'world' }) }}
+    <!--    hello_messege  world-->
+  </div>
+</template>
+
+//src/locales/en.json
+{
+"message": {
+  "hello": "hello_messege {name}"
+},
+"person": " {n} 00 | {n} 01 | {n} 02 | {n} 03"
+}
